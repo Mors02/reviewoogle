2a5662d8965a588b4a41cb86b80defedb288222f
Leonardo Morselli <leonardo.morselli02@gmail.com>
2024-04-02 16:01:10 +0200

    feat(Searcher): aggiunta ricerca base

---
+++ Searcher.py
@@ -0,0 +1,34 @@
+import operator
+from whoosh.index import open_dir
+from whoosh.fields import *
+from whoosh.qparser import QueryParser, syntax, query
+import os, os.path
+
+
+
+
+
+
+
+
+def base_search(indexPath, query_terms):
+    ix = open_dir(indexPath)
+
+    qp = QueryParser("review", schema=ix.schema, group=syntax.OrGroup)
+    tp = QueryParser("title", schema=ix.schema, group=syntax.OrGroup)
+
+    query_terms = u"dota best game"
+    q = qp.parse(query_terms)
+    tq = tp.parse(query_terms)
+    with ix.searcher() as searcher:
+        #SEARCH THE QUERY
+        review_results = searcher.search(q)
+        title_results = searcher.search(tq)
+        
+        #COMBINE BOTH
+        review_results.upgrade_and_extend(title_results)
+
+        #PRINT
+        for rev in review_results:
+            print(rev["title"] + ": " + rev["review"] + " | " + rev["review_votes"])
+
--- index.py
+++ index.py
@@ -1,11 +1,12 @@
 import Indexer
+import Searcher
 
 filename = './dataset/compact_dataset.csv'
 indexPath = './indexdir'
 
 
 #TO INDEX
-Indexer.index(filename, indexPath)
+#Indexer.index(filename, indexPath)
 
 #TO SEARCH (DIVIDED BY SEARCH TYPE)
-    #NOT YET IMPLEMENTED
+Searcher.base_search(indexPath)
