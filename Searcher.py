86d51c68b6c3131a96278642f2ce19d58f7d4c31
Leonardo Morselli <leonardo.morselli02@gmail.com>
2024-04-03 12:26:46 +0200

    feat(Searcher): aggiunta ricerca processata

--- Searcher.py
+++ Searcher.py
@@ -3,6 +3,7 @@ from whoosh.index import open_dir
 from whoosh.fields import *
 from whoosh.qparser import QueryParser, syntax, query
 import os, os.path
+import Preprocessor
 
 #DOCS:
 #https://whoosh.readthedocs.io/en/latest/searching.html
@@ -13,18 +14,44 @@ def base_search(indexPath, query_terms):
     qp = QueryParser("review", schema=ix.schema, group=syntax.OrGroup)
     tp = QueryParser("title", schema=ix.schema, group=syntax.OrGroup)
 
-    query_terms = u"dota best game"
-    q = qp.parse(query_terms)
+    #query_terms = u"best game dota"
+    q = qp.parse(query_terms)    
     tq = tp.parse(query_terms)
     with ix.searcher() as searcher:
         #SEARCH THE QUERY
-        review_results = searcher.search(q)
-        title_results = searcher.search(tq)
+        review_results = searcher.search(q, limit=50)
+        title_results = searcher.search(tq, limit=50)
         
         #COMBINE BOTH
         review_results.upgrade_and_extend(title_results)
 
         #PRINT
-        for rev in review_results:
-            print(rev["title"] + ": " + rev["review"] + " | " + rev["review_votes"])
+        #for rev in review_results:
+          #  print(str(rev["id"]) + " | " + rev["title"] + ": " + rev["review"])
+
+def processed_search(indexPath, query_terms):
+
+    print("RICERCA PROCESSATA")
+    ix = open_dir(indexPath)
+
+    processed_query = Preprocessor.process(query_terms, True, True)
+
+    qp = QueryParser("processed_review", schema=ix.schema, group=syntax.OrGroup)
+    tp = QueryParser("processed_title", schema=ix.schema, group=syntax.OrGroup)
+
+    #query_terms = u"best game dota"
+
+    q = qp.parse(" ".join(processed_query))    
+    tq = tp.parse(" ".join(processed_query))
+    with ix.searcher() as searcher:
+        #SEARCH THE QUERY
+        review_results = searcher.search(q, limit=50)
+        title_results = searcher.search(tq, limit=50)
+        
+        #COMBINE BOTH
+        review_results.upgrade_and_extend(title_results)
+
+        #PRINT
+        #for rev in review_results:
+         #   print(str(rev["id"]) + " | " + rev["title"] + ": " + rev["review"])
 
--- index.py
+++ index.py
@@ -1,12 +1,35 @@
 import Indexer
 import Searcher
+import time
 
 filename = './dataset/compact_dataset.csv'
 indexPath = './indexdir'
 
 
+def Timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print("RUN TIMER: " + str(end - start))
+    return wrapper
+
 #TO INDEX
 #Indexer.index(filename, indexPath)
 
 #TO SEARCH (DIVIDED BY SEARCH TYPE)
-Searcher.base_search(indexPath)
+@Timer
+def base_search():
+    Searcher.base_search(indexPath, u"should i play black ops 3?")
+
+@Timer
+def processed_search():
+    Searcher.processed_search(indexPath, u"should i play black ops 3?")
+
+
+base_search()
+processed_search()
+
+#TEST
+#should I play Dota2? -> base_search gives a lot of other games in the results, processed_search is better but slower
+#
